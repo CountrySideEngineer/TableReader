e33b54c032180c96d7119790966d79da7036afff
CountrySideEngineer <deep-sleeper@nifty.com>
2024-08-07 20:09:07 +0900

    Start developing TableReader dll for .NET environment.

---
+++ src/.NET/dev/TableReader/TableReader/TableData/Range.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableReader.TableData
+{
+	public class Range
+	{
+		/// <summary>
+		/// Table start row number.
+		/// </summary>
+		public int StartRow { get; set; } = 0;
+
+		/// <summary>
+		/// Table start column number.
+		/// </summary>
+		public int StartColumn { get; set; } = 0;
+
+		/// <summary>
+		/// The number of rows in table.
+		/// </summary>
+		public int RowCount { get; set; } = 0;
+
+		/// <summary>
+		/// The number of columns in table.
+		/// </summary>
+		public int ColumnCount { get; set; } = 0;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public Range() { }
+
+		/// <summary>
+		/// Copy constructor.
+		/// </summary>
+		/// <param name="src"></param>
+		public Range(Range src)
+		{
+			StartRow = src.StartRow;
+			StartColumn = src.StartColumn;
+			RowCount = src.RowCount;
+			ColumnCount = src.ColumnCount;
+		}
+	}
+}
---
+++ src/.NET/dev/TableReader/TableReader/interface/ITableReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using Range = TableReader.TableData.Range;
+
+namespace TableReader.Interface
+{
+	public interface ITableReader
+	{
+		DataTable Read(string name);
+
+		DataTable Read(string name, Range range);
+	}
+}
